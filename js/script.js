b732039670df3e2b5d33fa3de901461ae0757032
Ana Laura <arochadesouza692@gmail.com>
2025-08-25 10:32:46 -0300

    atualizacoes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar link do menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link){
@@ -9,3 +10,39 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink);
+
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto (parametro){
+    const elemento = document.getElementById(parametro);
+    if (elemento){
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+
+//Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas);
